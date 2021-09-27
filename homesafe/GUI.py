23d5684ec58c61850f89e138e3ec18257cd3de4d
Will DeBernardi <will@debernardi.us>
2021-09-26 18:29:51 -0600

    Refactor keypad, add entry box, add states

--- homesafe/GUI.py
+++ homesafe/GUI.py
@@ -1,39 +1,78 @@
 import tkinter as tk
-
-def main():
-    root = tk.Tk()
-    root.geometry("800x600")
-
-    numpad = NumPad(root)
-    root.mainloop()
+from tkinter.constants import END
 
 btn_list = [
-    '7', '8', '9',
+    '1', '2', '3',
     '4', '5', '6',
-    '1', '2', '3', 
+    '7', '8', '9', 
     'Del', '0', '*'
 ]
 
-class NumPad(tk.Frame):
-    def __init__(self, root):
-        tk.Frame.__init__(self, root)
-        self.grid()
-        self.numpad_create()
+entry_box = ""
+safe_state = ""
+door_state = ""
 
-    def handle_input(self):
-        print(self)
+def handle_input(val):
+    global entry_box, safe_state
+    entry_box.insert(END, val)
 
-    def numpad_create(self):
-        r = 1
-        c = 0
-        for b in btn_list:
-            cmd = lambda button=b: print(button)
-            self.b = tk.Button(self, text=b, width=10, command=cmd).grid(row=r, column=c)
-            c += 1
-            if c > 2:
-                c = 0
-                r += 1
+    if(len(entry_box.get()) == 4):
+        print("code entered")
+        # HANDLE ENTRY LOGIC HERE
+        safe_state.set("Safe is UNLOCKED")
+        door_state.set("Door is OPEN")
+
+def create_numpad(container):
+    frame = tk.Frame(container)
+
+    r = 1
+    c = 0
+    for b in btn_list:
+        cmd = lambda button=b: handle_input(button)
+        b = tk.Button(frame, text=b, width=10, command=cmd).grid(row=r, column=c)
+        c += 1
+        if c > 2:
+            c = 0
+            r += 1
+
+    return frame
+
+def create_entry(container):
+    global entry_box
+
+    frame = tk.Frame(container)
+    entry_box = tk.Entry(frame, width=30)
+    entry_box.grid(column=0, row=0)
+
+    return frame
+
+def main():
+    global safe_state, door_state
+
+    root = tk.Tk()
+    root.title=("Safe")
+    root.geometry("800x600")
+    root.columnconfigure(0, weight=1)
+    # root.rowconfigure(0)
+    # root.rowconfigure(1)
+    # root.rowconfigure(2)
+
+    entry_frame = create_entry(root)
+    entry_frame.grid(column=0, row= 0)
+
+    keypad_frame = create_numpad(root)
+    keypad_frame.grid(column=0, row =1)
+
+    safe_state = tk.StringVar();
+    safe_state.set("Safe is LOCKED")
+    tk.Label(root, textvariable=safe_state).grid(column=0, row=2)
+
+    door_state = tk.StringVar();
+    door_state.set("Door is CLOSED")
+    tk.Label(root, textvariable=door_state).grid(column=0, row=3)
+
 
     
+    root.mainloop()
 
 main()
