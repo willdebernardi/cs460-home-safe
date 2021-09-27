6c64aff57aeae81e2c55962741c0c211e53fc5a3
Nicholas Livingstone <nlivingsto@unm.edu>
2021-09-26 22:03:47 -0600

    Door, Timer, and Code Component API functional

--- homesafe/component_apis.py
+++ homesafe/component_apis.py
@@ -1,12 +1,53 @@
-    """[summary]
-    Contains components API classes and functions. 
-    """
-# def Door():
+from passlib.hash import pbkdf2_sha256 as encrypt
+import threading
+
+class Door:
+    def __init__(self):
+        self.open = False
+        self.locked = True
+        
+    def lock(self):
+        if(not self.open): # Only lock when the door is closed 
+            self.locked = True
+        
+    def unlock(self):
+        self.locked = False
+        
+    def open_door(self):
+        return self.open
     
-# def KeyPad():
+    def locked_door(self):
+        return self.locked
     
-# def Timer():
     
-# def SafeCode():
+class Timer:
+    def __init__(self):
+        self.timer = None
+        
+    def set_time(self, s, timeout_func, *args):
+        self.timer = threading.Timer(s, timeout_func, args)
+        self.timer.start()
+        
+    def reset(self):
+        self.timer.cancel()
+    
+class SafeCode:
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.pwd = encrypt.hash("0000")
+    
+    def test(self, pin):
+        return encrypt.verify(pin, self.pwd)
+    
+    def set_code(self, pin):
+        if len(pin) > 4:
+            raise AttributeError("PIN too long") 
+        if len(pin) < 1:
+            raise AttributeError("PIN to short")
+        self.pwd = encrypt.hash(pin)
+        
+        
     
 # def notifications():
